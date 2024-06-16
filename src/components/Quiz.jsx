3999128145421632f719203c7d4d93b71987d507
mcalsing <marcelocalsing@gmail.com>
2024-06-15 23:32:19 -0300

    Ao submeter o teste a tela de score é exibida, e o calculo do score é exibido corretamente com perguntas random. As respostas sao exibidas em lista na tela de score

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const TIMEFORTEST = 30*60;
+const TIMEFORTEST = 2*60;
 
 const Loading = () => (
   <div className="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center items-center border-2 rounded-tr-[50%] rounded-bl-[50%]">
@@ -31,12 +31,13 @@ const Quiz = () => {
   const [status, setStatus] = useState("");
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [showQuestion, setShowQuestion] = useState(true);
 
   useEffect(() => {
     fetch("/quiz.json")
       .then((response) => response.json())
       .then((data) => {
-        data.sort(() => Math.random() - 0.5);
+         data.sort(() => Math.random() - 0.5);
         setQuestions(data);
       })
       .catch((error) => console.error("Error fetching quiz data:", error));
@@ -72,21 +73,28 @@ const Quiz = () => {
     // Handle answer selection logic here
     const updatedAnswers = { ...answers, [questionId]: selectedOption };
     setAnswers(updatedAnswers);
+    console.log(updatedAnswers)
   };
 
   const handleNextQuestion = () => {
     if (currentQuestionIndex < questions.length -1) {
-       setIsButtonDisabled(currentQuestionIndex === questions.length - 2);
+      /* setIsButtonDisabled(currentQuestionIndex === questions.length - 2); */
       setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
-    } else {
+    } 
+    if ((currentQuestionIndex === questions.length - 2)) {
       setIsButtonDisabled(true);
-    }
+      if (timer === 0 ) {
+        setShowResult(true);
+      }
+    } 
+   /*  else {
+      setIsButtonDisabled(true);
+    } */
   }
 
   const handleSubmit = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+    // window.scrollTo({ top: 0, behavior: "smooth" });
     setLoading(true);
-
     clearInterval(timerIntervalId);
 
     // Calculate score and show result
@@ -97,17 +105,22 @@ const Quiz = () => {
       // Determine the status based on the percentage
       const newStatus = percentage >= 50 ? "Passed" : "Failed";
       setStatus(newStatus);
-
       setShowResult(true);
       setLoading(false);
-    }, 5000);
+      setShowQuestion(false)
+    }, 1000);
   };
 
   const calculateScore = (userAnswers) => {
-    const correctAnswers = questions.map((question) => question.answer);
+    //Faz a soma correta do score quando perguntas random
+    const questionAnswerMap = {};
+    for (const question of questions) {
+      questionAnswerMap[question.id] = question.answer;
+    }
     let score = 0;
     for (const questionId in userAnswers) {
-      if (userAnswers[questionId] === correctAnswers[questionId - 1]) {
+      const correctAnswer = questionAnswerMap[questionId];
+      if (userAnswers[questionId] === correctAnswer) {
         score++;
       }
     }
@@ -116,97 +129,121 @@ const Quiz = () => {
 
   // Reset states and reload the page
   const restartQuiz = () => {
+    setLoading(true);
     setAnswers({});
     setScore(0);
     setShowResult(false);
     setLoading(false);
     setTimer(TIMEFORTEST);
     navigate("/quiz");
+    setShowQuestion(true);
+    // Refaz a lista randomica de perguntas
+    fetch("/quiz.json")
+      .then((response) => response.json())
+      .then((data) => {
+        data.sort(() => Math.random() - 0.5);
+        setQuestions(data);
+      })
+      .catch((error) => console.error("Error fetching quiz data:", error));
   };
 
   return (
     <main>
-      <div className="flex justify-center gap-1 pt-3">
-        <p className="text-x1 text-slate-700">Tempo restante:</p>
-        <h1 className="text-green-700" id="count">
-          {formatTime(timer)}
-        </h1>
-      </div>
-      <div className="w-[800px] flex md:flex-row flex-col mx-auto">
-        {/* question section */}
-        <div className="w-full text-slate-700">
-          <div>
-            {questions.length > 0 && (
-              <div key={questions[currentQuestionIndex].id}
-                className="m-3 py-3 px-4 border border-slate-400 rounded"
-              >
-                <p className="flex rounded text-xl p-2">
-                  <span className="h-9 w-9 bg-sky-800 flex justify-center items-center text-slate-100 mr-3 p-3 rounded-full">
-                    {questions[currentQuestionIndex].id}
-                  </span>
-                  <p className="text-justify">{questions[currentQuestionIndex].question}</p>
-                </p>
-                <div className="grid grid-cols-2 gap-4 mt-5">
-                  {questions[currentQuestionIndex].options.map((option, index) => (
-                    <div
-                      className={`border border-gray-400 rounded p-5 cursor-pointer hover:bg-sky-800 hover:text-slate-300 
-                        ${answers[questions[currentQuestionIndex].id] === option ? "bg-sky-800 text-slate-100" : ""}`}
-                      key={option}
-                      onClick={() => handleAnswerSelect(questions[currentQuestionIndex].id, option)}
-                    >
-                      <p>{option}</p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
-            <button
-              onClick={handleNextQuestion}
-               disabled={isButtonDisabled}
-              className={`${isButtonDisabled ? "text-slate-400" : "text-slate-100"} bg-sky-800 px-6 py-2 m-3 rounded`}
-            >
-              Próxima Pergunta
-            </button>
-          </div>
+      {showQuestion && (
+        <div className="flex justify-center gap-1 pt-3">
+          <p className="text-x1 text-slate-700">Tempo restante:</p>
+          <h1 className="text-green-700" id="count">
+            {formatTime(timer)}
+          </h1>
         </div>
-
-        {/* answer  section*/}
-        { <div className="md:w-[30%] w-full p-4 bg-slate-200">
-          {showResult && (
+      )}
+      <div className="w-[800px] mx-auto">
+        {/* question section */}
+        {showQuestion && (
+          <div className="w-full text-slate-700">
             <div>
-              <h3 className="text-2xl font-medium">Your Score: </h3>
-              <div className="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center items-center border-2 rounded-tr-[50%] rounded-bl-[50%]">
-                <h3
-                  className={`text-xl ${
-                    status === "Passed" ? "text-green-800" : "text-red-500"
-                  }`}
+              {questions.length > 0 && (
+                <div key={questions[currentQuestionIndex].id}
+                  className="m-3 py-3 px-4 border border-slate-400 rounded"
                 >
-                  {status}
-                </h3>
-                <h1 className="text-3xl font-bold my-2">
-                  {score}
-                  <span className="text-slate-800">/30</span>
-                </h1>
-                <p className="text-sm flex justify-center items-center gap-2">
-                  Total Time:{" "}
-                  <span className="text-xl text-orange-500">
-                    {formatTime(TIMEFORTEST - timer)}
-                    <span className="text-xs">sec</span>
-                  </span>
-                </p>
-              </div>
+                  <p className="flex rounded text-xl p-2">
+                    <span className="h-9 w-9 bg-sky-800 flex justify-center items-center text-slate-100 mr-3 p-3 rounded-full">
+                      {questions[currentQuestionIndex].id}
+                    </span>
+                    <p className="text-justify">{questions[currentQuestionIndex].question}</p>
+                  </p>
+                  <div className="grid grid-cols-2 gap-4 mt-5">
+                    {questions[currentQuestionIndex].options.map((option, index) => (
+                      <div
+                        className={`border border-gray-400 rounded p-5 cursor-pointer hover:bg-sky-800 hover:text-slate-300 
+                          ${answers[questions[currentQuestionIndex].id] === option ? "bg-sky-800 text-slate-100" : ""}`}
+                        key={option}
+                        onClick={() => handleAnswerSelect(questions[currentQuestionIndex].id, option)}
+                      >
+                        <p>{option}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
               <button
-                onClick={restartQuiz}
-                className="bg-[#FCC822] text-white w-full py-2 rounded mt-16"
+                onClick={handleNextQuestion}
+                disabled={isButtonDisabled}
+                className={`${isButtonDisabled ? "text-slate-400" : "text-slate-100"} bg-sky-800 px-6 py-2 m-3 rounded`}
               >
-                Restart
+                Próxima Pergunta
+              </button>
+              <button
+                onClick={handleSubmit} 
+                className="text-slate-100 bg-sky-800 px-6 py-2 m-3 rounded"
+              >
+                Submit
               </button>
             </div>
-          )}
-          {loading && <Loading />}
-        </div>
-        }     
-      </div>
+          </div>
+        )}
+        {/* answer  section*/}
+        {showResult && (
+          <div className="bg-sky-400">
+            <h3 className="text-2xl font-medium">Your Score: </h3>
+            <div className="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center items-center border-2 rounded-tr-[50%] rounded-bl-[50%]">
+              <h3
+                className={`text-xl ${
+                  status === "Passed" ? "text-green-800" : "text-red-500"
+                }`}
+              >
+                {status}
+              </h3>
+              <h1 className="text-3xl font-bold my-2">
+                {score}
+                <span className="text-slate-800">/10</span>
+              </h1>
+              <p className="text-sm flex justify-center items-center gap-2">
+                Total Time:{" "}
+                <span className="text-xl text-orange-500">
+                  {formatTime(TIMEFORTEST - timer)}
+                  <span className="text-xs">sec</span>
+                </span>
+              </p>
+            </div>
+            <button
+              onClick={restartQuiz}
+              className="bg-[#FCC822] text-white w-full py-2 rounded mt-16"
+            >
+              Restart
+            </button>
+            <h2>Your Answers:</h2>
+            <ul>
+              {Object.keys(answers).map((key) => (
+                <li key={key} className="text-base mb-2">
+                  {key}: {answers[key]}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {loading && <Loading />}
+      </div> 
     </main>
   );
 };
